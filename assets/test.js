7f3c39c2f3704651bf7ec7d155043ec55f48b25d
Nicki Lindstrom <lindstromn13@gmail.com>
2024-02-14 09:20:40 -0700

    Adding button

--- assets/test.js
+++ assets/test.js
@@ -1,1 +1,9 @@
 console.log("this is a test");
+
+let ele = document.getElementsById("my-ele");
+let button = document.createElement("button");
+button.title = "Testing";
+button.addEventListener(() => {
+    console.log("button clicked");
+});
+ele.appendChild(button);
